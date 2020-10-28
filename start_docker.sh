37b518c8dc51b1f730dbf62cf3c1b8e560925d53
hotgun6666 <hotgun6666@gmail.com>
2020-10-28 22:50:47 +0700

    Fix CD config #CA-27

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,6 @@
 # setup config.py file
-cp src/config/settings/config.template.py src/config/settings/config.py
+yes | cp -i src/config/settings/config.template.py src/config/settings/config.py
 # create folder for db
-mkdir pgdata
+mkdir -p pgdata
 # start docker-compose
 UID_GID="$(id -u):$(id -g)" docker-compose up --build -d
